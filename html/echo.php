c701958ea198a12e612b4586851e7b8e5d903cbb
Valentine Gutkin <admin@networklabs.info>
2019-05-14 11:27:28 +0300

    Create echo.php

---
+++ html/echo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Web Info</title>
+	</head>
+	<body>
+		<img src="./images/rad.jpg" width="150" height="40">
+		<br><br>
+		<?php 
+			echo ( "Your Messege was: <br><br><textarea style='width:400px; height:400px;' > " . $_POST['Msg'] ."</textarea><br>\n"); 
+		?>
+		<a href="/index.html"> Go Back! </a>
+	</body>
+</html>
